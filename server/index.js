9e5d60b232d2c9182b5aaaee7b8a93365b1f50f7
DivineKushaL <divine.kushalpatel28@gmail.com>
2024-06-28 02:08:06 +0530

    register page frontend done

---
+++ server/index.js
@@ -0,0 +1,35 @@
+//adding the plateform for making of websites
+const express = require("express");
+//adding cors to deal with diffrent local host like 3000 3001  
+const cors = require("cors");
+const mongoose = require("mongoose");
+//add all routes to this page
+
+//stating app on express plateform 
+const app = express();
+require("dotenv").config();
+
+//middlewares used 
+app.use(cors());
+app.use(express.json());
+
+const MONGO_URL = process.env.MONGO_URL;
+
+//to connect the mongodb database
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+
+  const PORT=5000 || process.env.PORT;
+  const server = app.listen(PORT, () =>
+    console.log(`Server started on ${PORT}`)
+  );
