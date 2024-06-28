718d8aa60f203558534e5176c98cdd35f3b05bd6
Ayush <aayush200509@gmail.com>
2024-06-28 14:15:17 +0530

    chat page linked with avatar page and getallusers controller done

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { allUsersRoute} from "../utils/APIRoutes";
+import ChatContainer from "../components/ChatContainer";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
-function Chat() {
+export default function Chat() {
+  const navigate = useNavigate();
+  //stores all the contacts which are online or login
+  const [contacts, setContacts] = useState([]);
+  //stores selected chat that we have to how on the right side 
+  const [currentChat, setCurrentChat] = useState(undefined);
+   //for footer we have to name current user 
+   const [currentUser, setCurrentUser] = useState(undefined);
+
+  //for fetching the user from local storage   
+  useEffect(() => {
+    const checkLocalStorage = async () => {
+      const userKey = "chat-app-user";
+      const userItem = localStorage.getItem(userKey);
+
+      if (!userItem) {
+        //if user not found then navigate to the login page
+        navigate("/login");
+      } else {
+        //else set the current user in usestate
+        const user = await JSON.parse(userItem);
+        setCurrentUser(user);
+      }
+    };
+    //first form a function and call it dont write it in one go 
+    checkLocalStorage();
+  }, [navigate]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!currentUser) return;
+
+        if (!currentUser.isAvatarImageSet) {
+          navigate("/setAvatar");
+          return;
+        }
+
+        const response = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [currentUser, navigate]);
+
   return (
-    <div>
-      <h1>hii</h1>
-    </div>
-  )
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts}  />
+        {currentChat === undefined ? (
+          <Welcome />
+        ) : (
+          <ChatContainer currentChat={currentChat} />
+        )}
+      </div>
+    </Container>
+  );
 }
 
-export default Chat
+const Container = styled.div`
+   height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #282c34;
+  .container {
+    height: 85vh;
+    width: 65vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    // media query 
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
+
