ca41c6ec2da1e160560f827c39bf0a9fcbeea0b8
Ayush <aayush200509@gmail.com>
2024-06-29 01:10:53 +0530

    chatinput and chatcontainer controller done without emoji picker

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,17 @@
 const {
-    login,
-    register,
-    setAvatar,
-    getAllUsers
-  } = require("../controllers/userController");
-  
-  const router = require("express").Router();
-  
-  router.post("/login", login);
-  router.post("/register", register);
-  router.post("/setavatar/:id", setAvatar);
-  router.get("/allusers/:id", getAllUsers);
-  
-  module.exports = router;
+  login,
+  register,
+  getAllUsers,
+  setAvatar,
+  logOut,
+} = require("../controllers/userController");
+
+const router = require("express").Router();
+
+router.post("/login", login);
+router.post("/register", register);
+router.get("/allusers/:id", getAllUsers);
+router.post("/setavatar/:id", setAvatar);
+router.get("/logout/:id", logOut);
+
+module.exports = router;
