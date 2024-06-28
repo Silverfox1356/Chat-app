85bacb109a7b4d907f04410e7090e7298ada6af2
DivineKushaL <divine.kushalpatel28@gmail.com>
2024-06-28 14:15:17 +0530

    chat page linked with avatar page and getallusers controller done

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -3,3 +3,6 @@ export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
+
+
+export const allUsersRoute = `${host}/api/auth/allusers`;
