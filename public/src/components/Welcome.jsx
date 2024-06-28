8f16c7c2d2c83f615afcdab5de472d5a9ee336b7
DivineKushaL <divine.kushalpatel28@gmail.com>
2024-06-28 17:51:58 +0530

    welcome controller done

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
 
-function Welcome() {
+  //fetching the username from local storage  
+  useEffect(() => {
+    const fetchData = async ()=>{
+        const data=await JSON.parse(
+            localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+          ).username  ;
+    setUserName(data);
+}
+fetchData();
+  }, []);
+
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Container>
+      <img src={Robot} alt="" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
 }
 
-export default Welcome
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: #20232a;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #5b8690;
+  }
+`;
