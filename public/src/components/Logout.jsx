d7fd114625805838876b02af95be724102c7f77d
Ayush <aayush200509@gmail.com>
2024-06-29 15:04:05 +0530

    logout done with controller

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BiPowerOff } from "react-icons/bi";
+import styled from "styled-components";
+import axios from "axios";
+import { logoutRoute } from "../utils/APIRoutes";
+export default function Logout() {
+  const navigate = useNavigate();
 
-function Logout() {
+  
+  const handleClick = async () => {
+    const id = await JSON.parse(
+      localStorage.getItem("chat-app-user")
+    )._id;
+    const data = await axios.get(`${logoutRoute}/${id}`);
+    if (data.status === 200) {
+      localStorage.clear();
+      navigate("/login");
+    }
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <Button onClick={handleClick}>
+      <BiPowerOff />
+    </Button>
+  );
 }
 
-export default Logout
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #5b8690;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.5rem;
+    // color: #ebe7ff;
+  }
+`;
